4cb1200626c38099a02240789f5635fc34db3f6b
Vaishnavi Newalkar <vaishnavinewalkar11@gmail.com>
2026-03-30 19:34:02 +0530

    Add: find-duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        // Phase 1: Detect cycle
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        // Phase 2: Find cycle entry (duplicate)
+        int ptr1 = nums[0];
+        int ptr2 = slow;
+
+        while (ptr1 != ptr2) {
+            ptr1 = nums[ptr1];
+            ptr2 = nums[ptr2];
+        }
+
+        return ptr1;
+    }
+};
