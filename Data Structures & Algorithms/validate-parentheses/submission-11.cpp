e8b29bfd196674d538edc1709be384104bb71c30
Vaishnavi Newalkar <vaishnavinewalkar11@gmail.com>
2026-04-06 19:52:44 +0530

    Add: validate-parentheses - submission-11

---
+++ Data Structures & Algorithms/validate-parentheses/submission-11.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string str) {
+        stack<char> st;
+        if(str.size()==1) return false;
+        for(char s:str){
+            if(s=='[' || s=='{' || s=='('){
+                st.push(s);
+            }
+            else {
+                if(st.empty()) return false;
+                if(s==']' && st.top()!='[' ||
+                s=='}' && st.top()!='{'||
+                s==')' && st.top()!='(') return false;
+                // if(s==']' && st.top()=='[' ||
+                // s=='}' && st.top()=='{'||
+                // s==')' && st.top()=='('){
+                // st.pop();
+                // }
+                st.pop();
+            }
+
+        }
+        return st.empty();
+    }
+};
